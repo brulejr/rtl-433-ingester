83b6f6eac03600069e53b86f61b0aa22a07e8c25
Jon Brule <brulejr@tgmail.com>
2025-01-30 21:42:12 -0500

    Adds Schrader data summary

--- src/main/kotlin/io/jrb/labs/rtl433/ingester/service/aggregator/DataSummary.kt
+++ src/main/kotlin/io/jrb/labs/rtl433/ingester/service/aggregator/DataSummary.kt
@@ -2,22 +2,33 @@ package io.jrb.labs.rtl433.ingester.service.aggregator
 
 import io.jrb.labs.rtl433.ingester.model.DscSecurity
 import io.jrb.labs.rtl433.ingester.model.Rtl433Data
+import io.jrb.labs.rtl433.ingester.model.SchraderTpms
 
 class DataSummary {
 
     val dscSecurity: CollectionSummary = CollectionSummary()
+    val schraderTpms: CollectionSummary = CollectionSummary()
 
     fun aggregate(data: Rtl433Data) {
         when (data) {
-            is DscSecurity -> processDscSecurity(data)
+            is DscSecurity -> processData(data)
+            is SchraderTpms -> processData(data)
         }
     }
 
-    private fun processDscSecurity(message: DscSecurity) {
+    private fun processData(message: DscSecurity) {
         if (message.device?.type == "UNKNOWN") {
             dscSecurity.unknown(message.id!!)
         } else {
             dscSecurity.known(message.device?.name!!)
+        }
+    }
+
+    private fun processData(message: SchraderTpms) {
+        if (message.device?.type == "UNKNOWN") {
+            schraderTpms.unknown(message.id!!)
+        } else {
+            schraderTpms.known(message.device?.name!!)
         }
     }
 
